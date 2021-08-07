f3ac057fada9b09880cde3d17041cf6bf2fde586
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2021-08-07 14:01:22 +0300

    fix abraham script

--- abraham_r.R
+++ abraham_r.R
@@ -1,1 +1,14 @@
-print("Abraham Adeyemo, Abrahamrocketmail2@gmail.com, @Abraham4, genomics, vaccine informatics")
+def humming(a, b):
+	distance = 0
+	for i in length(a):
+		if a[i] != b[i]: distance += 1
+	return distance
+
+name = "Abraham Adeyemo"
+email = "Abrahamrocketmail2@gmail.com"
+slack = "@Abraham4"
+biostack = "genomics"
+twitter = "@Abraham4"
+humming_distance = humming(slack, twitter)
+
+print(name, email, slack, biostack, twitter, humming_distance, separator=',')
