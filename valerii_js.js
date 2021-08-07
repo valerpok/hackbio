18661502ca9bf6084deff344284b3e179d3759f7
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2021-08-07 14:24:19 +0300

    Add valerii script

---
+++ valerii_js.js
@@ -0,0 +1,12 @@
+function humming(a, b) {
+	distance = 0
+	for (let i = 0; i < a.length; i++) {
+  		if (a[i]	!= b[i]){distance+=1}
+	}
+	return distance
+}
+
+slack = "@Valerii"
+twitter = "@__Immo"
+humm = humming(slack, twitter)
+console.log('Pokrytiuk Valerii, valer.pok@gmail.com, @Valerii, genomics, @__Immo, ' + humm)
